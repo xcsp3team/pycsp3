d35ee9710fe82535304939a0f5b66a0a2c2718b5
lecoutre <lecoutre@cril.fr>
2019-12-22 11:05:01 +0100

    Refactoring of models

--- problems/g6_tests/TestSolver.py
+++ problems/g6_tests/TestSolver.py
@@ -3,27 +3,22 @@ from pycsp3.solvers.abscon import AbsConProcess, AbsconPy4J
 
 x = VarArray(size=[10, 10], dom=range(150))
 y = VarArray(size=[10, 10], dom=range(150))
-z = VarArray(size=[10], dom=range(150))
+z = VarArray(size=10, dom=range(150))
 
 satisfy(
     min(abs(x[0][0]), abs(y[0][0])),
     max(abs(x[0][0]), min(y[0], z[1]))
 )
 
-print("Compile:")
-print("")
-xcsp = compile()
+print("Compile:\n")
+instance = compile()
 
-print("")
-print("Static solving:")
-print("")
-solution = AbsConProcess().solve(xcsp)
+print("\nStatic solving:\n")
+solution = AbsConProcess().solve(instance)
 print("solution:", solution)
 
-print("")
-print("Py4j solving:")
-print("")
+print("\nPy4j solving:\n")
 solver = AbsconPy4J()
-solver.loadXCSP3(xcsp)
+solver.loadXCSP3(instance)
 
 print("in progress")
