36e8f0ebcdc4b5f35a615ad45960e8406c121dc2
lecoutre <lecoutre@cril.fr>
2019-12-24 12:25:10 +0100

    guide (new constraints described)

--- problems/g1_single/Purdey.py
+++ problems/g1_single/Purdey.py
@@ -5,11 +5,11 @@ from pycsp3 import *
  Each family bought a different item: 50 pounds of flour, two gallons of kerosene, ten yards of muslin cloth, ten pounds of sugar.
  One family paid cash, one took the item on credit and the other two traded other items for it (one a cured ham and the other a bushel of peas).
  We know that:
-  - The Boyds were new in town, and this was their first visit to the store.
-  - The family (which wasn't the Logans) that traded the bushel of peas didn't buy the kerosene.
-  - The Boyds and the Garveys bought the kerosene and the muslin in some order.
-  - One family traded a cured ham for a large sack of flour.
-  - Purdey only extended credit to regular customers, such as the family that bought the muslin on credit.
+  - the Boyds were new in town, and this was their first visit to the store
+  - the family (which wasn't the Logans) that traded the bushel of peas didn't buy the kerosene
+  - the Boyds and the Garveys bought the kerosene and the muslin in some order
+  - one family traded a cured ham for a large sack of flour
+  - Purdey only extended credit to regular customers, such as the family that bought the muslin on credit
 """
 
 Boyds, Garveys, Logans, Navarros = families = ["Boyds", "Garveys", "Logans", "Navarros"]
