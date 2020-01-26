7711ff967958049543c93d29e8f1f966e69bb7b5
Nicolas <szczepanski.nicolas@gmail.com>
2020-01-26 19:40:13 +0100

    Problem with -solve

--- solvers/solver.py
+++ solvers/solver.py
@@ -95,7 +95,7 @@ class SolverProcess:
             return False
 
     def solution(self):
-        if self.stdout.find("<unsatisfiable") != -1:
+        if self.stdout.find("<unsatisfiable"):
             return Instantiation("unsatisfiable", "None", "None")
         left, right = self.stdout.find("<instantiation"), self.stdout.find("</instantiation>")
         root = etree.fromstring(self.stdout[left:right + len("</instantiation>")], etree.XMLParser(remove_blank_text=True))
