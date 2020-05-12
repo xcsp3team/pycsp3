964b6ce6cf94a9f3a7e0895cb05e7ad5d1bd6886
lecoutre <lecoutre@cril.fr>
2020-05-12 17:07:12 +0200

    New versions (jars) for Abscon and Tools

--- solvers/solver.py
+++ solvers/solver.py
@@ -19,7 +19,7 @@ def directory_of_solver(name):
 
 def class_path_abscon():
     d = directory_of_solver("abscon")
-    return d + "AbsCon-20-01.jar" + os.pathsep + d + "xcsp3-tools-1.1.1-SNAPSHOT.jar" + os.pathsep + d + "javax.json-1.0.4.jar"
+    return d + "AbsCon-20-05.jar" + os.pathsep + d + "xcsp3-tools-1.2.1-SNAPSHOT.jar" + os.pathsep + d + "javax.json-1.0.4.jar"
 
 
 def class_path_chocosolver():
