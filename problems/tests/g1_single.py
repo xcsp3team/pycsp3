e7cf80b1b2261209b6540e377def11f39d0333d4
lecoutre <lecoutre@cril.fr>
2020-06-29 08:55:48 +0200

    Towards a unique interface for embedded solvers

--- problems/tests/g1_single.py
+++ problems/tests/g1_single.py
@@ -19,22 +19,22 @@ same = (Tester(NAME)
         )
 
 xcsp = (Tester(NAME)
-        .add("Agatha")
-        .add("Allergy")
-        .add("Alpha")
-        .add("Alpha", variant="var")
-        .add("LabeledDice")
-        .add("NFractions")
-        .add("Photo")
-        .add("Photo", variant="aux")
-        .add("Picnic")
-        .add("Purdey")
-        .add("Recipe")
-        .add("Sandwich")
-        .add("SendMore")
-        .add("TrafficLights")
-        .add("Witch")
-        .add("Zebra")
+        .add("Agatha")  # 4 solutions
+        .add("Allergy")  # 1 solution
+        .add("Alpha")  # 1 solution
+        .add("Alpha", variant="var")  # 1 solution
+        .add("LabeledDice")  # 48 solutions
+        .add("NFractions")  # 22 solutions
+        .add("Photo")  # optimum 2
+        .add("Photo", variant="aux")  # optimum 2
+        .add("Picnic")  # 1 solution
+        .add("Purdey")  # 1 solution
+        .add("Recipe")  # optimum 1700
+        .add("Sandwich")  # 8 solutions
+        .add("SendMore")  # 1 solution
+        .add("TrafficLights")  # 4 solutions
+        .add("Witch")  # optimum 1300
+        .add("Zebra")  # 48 solutions
         )
 
 run(xcsp, diff, same)
