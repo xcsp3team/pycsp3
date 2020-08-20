d2e31c6a2596e4948fa7021682067fd4b8b3b80a
nicolas.szczepanski <szczepanski.nicolas@gmail.com>
2020-08-20 17:11:32 +0200

    UnitTestingExtension.py now works

--- functions.py
+++ functions.py
@@ -116,6 +116,7 @@ def VarArray(*, size, dom, comment=None):
 
 def _bool_interpretation_for_in(left_operand, right_operand, bool_value):
     assert type(bool_value) is bool
+        
     if isinstance(left_operand, Variable) and isinstance(right_operand, (tuple, list, set, frozenset, range)) and len(right_operand) == 0:
         return None
     if isinstance(left_operand, (Variable, int, str)) and isinstance(right_operand, (set, frozenset, range)):
@@ -130,6 +131,7 @@ def _bool_interpretation_for_in(left_operand, right_operand, bool_value):
     elif isinstance(left_operand, int) and (is_1d_list(right_operand, Variable) or is_1d_tuple(right_operand, Variable)):
         ctr = Count(right_operand, value=left_operand, condition=(TypeConditionOperator.GE, 1))  # atLeast1 TODO to be replaced by a member/element constraint ?
     else:  #  It is a table constraint
+        
         if not hasattr(left_operand, '__iter__'):
             left_operand = [left_operand]
         if not bool_value and len(right_operand) == 0:
--- problems/g6_testing/UnitTestingExtension.py
+++ problems/g6_testing/UnitTestingExtension.py
@@ -6,10 +6,11 @@ x = VarArray(size=[n, n], dom=range(1, n + 1))
 table = list(permutations(range(1, n + 1)))
 
 try:
-    x in table
+    satisfy(x in table)
 except AssertionError as e:
-    print("Scope and tuples not compatible:", e)
-
+    print("Wrong test 1: Scope and tuples not compatible:", e)
+    curser.queue_in.clear()
+    
 satisfy(
     # supports
     x[0] in table,
@@ -17,53 +18,61 @@ satisfy(
     # conflicts
     x[0] not in table
 )
+
 try:
     satisfy([1] * len(x[0]) in table)
-except TypeError as e:
-    print("Parameter 1:", e)
-
+except AssertionError as e:
+    print("Wrong test 2: Parameter 1:", e)
+    curser.queue_in.clear()
+    
 try:
     satisfy(x[0] in None)
 except TypeError as e:
-    print("Parameter 1:", e)
-
+    print("Wrong test 3: Parameter 2:", e)
+    curser.queue_in.clear()
+    
 table[1] = (1, 2)
-satisfy(
-    # Wrong but too costly to check all lengths
-    x[0] in table
-)
-
+try:
+    satisfy(x[0] in table)
+except AssertionError as e:
+    print("Wrong test 4: Scope and tuples not compatible:", e)
+    curser.queue_in.clear()
+    
 table = list(permutations(range(1, n + 1)))
 table[1] = ("a", "b", "c", "d", "e")
 
 try:
     satisfy(x[0] in table)
 except TypeError as e:
-    print("Table with different types:", e)
+    print("Wrong test 5: Table with different types:", e)
+    curser.queue_in.clear()
+    
 
 table = [(1, 2, 3, 4, 5), (0.1, 0.2, 0.3, 0.4, 0.5)]
-satisfy(
-    #  float and int in table
-    x[0] in table
-)
 
-satisfy(
-    # supports
-    x[0] in table,
+try:    
+    satisfy(
+        # supports
+        x[0] in table,
 
-    # conflicts
-    x[0] not in table
-)
-
+        # conflicts
+        x[0] not in table
+    )
+except AssertionError as e:
+    print("Wrong test 6: Table with different types:", e)
+    curser.queue_in.clear()
+    
 try:
     satisfy(x[0][0] in table)
 except AssertionError as e:
-    print("Scope and tuples not compatible:", e)
+    print("Wrong test 7: Scope and tuples not compatible:", e)
+    curser.queue_in.clear()
 
 try:
     satisfy(x[0] in table[0])
 except AssertionError as e:
-    print("Bad type for operand of in:", e)
+    print("Wrong test 8: Bad type for operand of in:", e)
+    curser.queue_in.clear()
 
 # TODO
 satisfy(
