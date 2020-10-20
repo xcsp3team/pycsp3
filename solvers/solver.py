afedd2e36d59e7f7303e6dfd50be31180623d2ff
nicolas.szczepanski <szczepanski.nicolas@gmail.com>
2020-10-20 10:58:55 +0200

    Get good parameters when solve() is call inside the model + solver.log is now write gradually

--- solvers/solver.py
+++ solvers/solver.py
@@ -108,9 +108,12 @@ class Logger(object):
         if os.path.exists(self.log_file):
             os.remove(self.log_file)
         self.log = open(self.log_file, "a")
+        self.log.close()
 
     def write(self, message):
-        self.log.write(message)  
+        self.log = open(self.log_file, "a")
+        self.log.write(message)
+        self.log.close()  
 
     def read(self):
         o = open(self.log_file, "r")
@@ -227,8 +230,10 @@ class SolverProcess:
 
         if compiler is False: # To get options from the model
             string_options = "[" + self.name.lower() + "," + string_options + "]"
-            solver, dict_options, dict_simplified_options = process_options(string_options)
-
+            solver, tmp_dict_options, tmp_dict_simplified_options = process_options(string_options)
+            dict_simplified_options.update(tmp_dict_simplified_options) 
+            dict_options.update(tmp_dict_options)
+            
         stopwatch = Stopwatch()
         solver_args = self.parse_general_options(string_options, dict_options, dict_simplified_options)
         solver_args += " " + dict_options["args"] if "args" in dict_options else ""
