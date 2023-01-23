e07950cb49d4ceba73ec0329f0a000d024563c11
lecoutre <lecoutre@cril.fr>
2023-01-23 11:09:30 +0100

    New version of ACE

--- tools/compactor.py
+++ tools/compactor.py
@@ -264,7 +264,8 @@ def _compact_constraint_arguments(arguments):
     for arg in list(arguments.values()):
         if isinstance(arg.content, list) and len(arg.content) > 0 and arg.content_compressible:
             if not isinstance(arg.content[0], list):  # It is only one list
-                if isinstance(arg.content[0], int) and str(arg.name) in ["coeffs", "values", "sizes", "lengths", "heights"]:  # TODO other arguments?
+                if isinstance(arg.content[0], int) and str(arg.name) in ["coeffs", "values", "sizes", "lengths", "heights", "weights", "balance"]:
+                    # TODO still other arguments to be added?
                     arg.content = _compact_values(arg.content, 3)
                 else:
                     arg.content = compact(arg.content, preserve_order=arg.content_ordered)
