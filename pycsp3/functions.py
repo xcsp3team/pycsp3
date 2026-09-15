4545d086049173182e4029f17f2922de54eb45c0
Szczepanski Nicolas <szczepanski.nicolas@gmail.com>
2026-09-15 11:41:20 +0200

    Revert the conjunction of binary equivalences for iff() with more than two arguments
    This reverts commit 46c1c4d: iff() with more than two arguments generates again the XCSP3 predicate
    iff with all its arguments, since the problem does not come from PyCSP3 but from the solvers that do
    not evaluate it as expected (all the arguments being equivalent, as ACE does). The test of this case
    is marked again for CHOCO, now with the issue chocoteam/choco-solver#1248, and for cosoco
    (xcsp3team/cosoco#74).
    
    Refs #102

--- pycsp3/functions.py
+++ pycsp3/functions.py
@@ -1319,10 +1319,7 @@ def iff(*args):
     if res is None:
         return Iff(*args, meta=True)
     res = [v if not isinstance(v, (tuple, list)) else v[0] if len(v) == 1 else conjunction(v) for v in res]
-    if len(res) == 2:
-        return res[0] == res[1]
-    # all the arguments must be equivalent: a conjunction of binary equivalences avoids the ambiguity of iff with more than two arguments
-    return Node.build(TypeNode.AND, *(Node.build(TypeNode.IFF, res[i], res[i + 1]) for i in range(len(res) - 1)))
+    return res[0] == res[1] if len(res) == 2 else Node.build(TypeNode.IFF, *res)
 
 
 def imply(*args):
